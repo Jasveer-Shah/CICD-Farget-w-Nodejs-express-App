dff79c2412b658f4394b75f242624d37d80cbeb7
Jasveer-Shah <talkjasveer@gmail.com>
2023-12-26 21:58:04 -0500

    add users.jade, make changes in index.jade and style.css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nodejs-Express App' });
 });
+
+router.get('/users', (req, res, next)=>{
+  res.render("users", { title: 'Nodejs-Express App' })
+})
+
+router.get('/users/details', (req, res, next)=>{
+  res.send("detail")
+})
+
 
 module.exports = router;
